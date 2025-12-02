5fc56c27f63efd3dc08904af1040240c0ae39cfd
carlosoo7 <Carlosfer047@gmail.com>
2025-12-02 08:04:20 -0500

    Pagina inicial
    Pagina base del proyecto

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navbar = document.getElementById("vars");
+
+// Posición inicial del navbar
+const navbarOffsetTop = navbar.offsetTop;
+
+// Función que fija el navbar cuando se hace scroll
+function fixNavbarOnScroll() {
+  if (window.scrollY >= navbarOffsetTop) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+}
+// Evento de scroll
+window.addEventListener("scroll", fixNavbarOnScroll);
+
+
