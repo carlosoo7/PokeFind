c5b664732d0713a599855eacc97ac8d814afa169
carlosoo7 <Carlosfer047@gmail.com>
2025-12-03 10:18:20 -0500

    Añadiendo funciones de prueba api
    Probando api con los botones de búsqueda de pokemons

--- js/script.js
+++ js/script.js
@@ -16,5 +16,35 @@ function fixNavbarOnScroll() {
 // Evento de scroll
 window.addEventListener("scroll", fixNavbarOnScroll);
 
+async function buscarPorNombre() {
+  const seachName= document.getElementById("searchName").value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${seachName}`;
+  const res = await fetch(url);
 
+  if (!res.ok) {
+    console.log("Pokémon no encontrado");
+    return;
+  }
+
+  const data = await res.json();
+
+  console.log("ID:", data.id);
+  console.log("Nombre:", data.name);
+}
+
+async function buscarPorId() {
+  const seachId= document.getElementById("searchId").value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${seachId}`;
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    console.log("Pokémon no encontrado");
+    return;
+  }
+
+  const data = await res.json();
+
+  console.log("ID:", data.id);
+  console.log("Nombre:", data.name);
+}
 
